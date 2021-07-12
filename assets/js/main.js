43b3caba488e1acb2b390849d4918a4f9697409e
OmarAliSoliman <omarasmar9955@gmail.com>
2021-07-12 19:39:35 +0200

    create menue and product page

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,6 +170,19 @@ $(document).ready(function () {
       $(".personal-img-validation-delete").css("display", "none");
     });
   }
+
+  if ($("#tile-1").length) {
+    $("#tile-1 .nav-tabs a").click(function () {
+      var position = $(this).parent().position();
+      var width = $(this).parent().width();
+      $("#tile-1 .slider").css({ left: +position.left, width: width });
+    });
+    var actWidth = $("#tile-1 .nav-tabs").find(".active").parent("li").width();
+    var actPosition = $("#tile-1 .nav-tabs .active").position();
+    $("#tile-1 .slider").css({ left: +actPosition.left, width: actWidth });
+  }
+
+
 });
 
 function openNav() {
